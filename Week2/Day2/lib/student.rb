1562dd5cea79eb340edf9e458c42b464d9fe604a
aminhp93 <minhpn.org.ec@gmail.com>
2016-05-01 01:50:07 -0700

    first commit

---
+++ Week2/Day2/lib/student.rb
@@ -0,0 +1,73 @@
+
+# class Student
+# 	attr_reader :first_name, :last_name, :courses
+
+# 	def initialize(first_name, last_name)
+# 		@first_name = first_name.capitalize
+# 		@last_name = last_name.capitalize
+# 		@courses = []
+# 	end
+
+# 	def name 
+# 		"#{first_name} #{last_name}"
+# 	end
+
+# 	def enroll(course)
+# 		return if self.courses.include?(course)
+# 		raise "conflict" if has_conflict?(course)
+
+# 		self.courses << course
+# 		course.students << self
+# 	end
+
+# 	def course_load
+# 		load = Hash.new(0)
+# 		self.courses.each do |i|
+# 			load[i.department] += i.credits
+# 		end
+# 		load
+# 	end
+
+# 	def has_conflict?(new_course)
+# 		self.courses.any? do |i|
+# 			new_course.conflicts_with?(i)
+# 		end
+# 	end
+# end
+
+class Student
+
+	attr_reader :first_name, :last_name, :courses
+
+	def initialize(first_name, last_name)
+		@first_name, @last_name = first_name, last_name
+		@courses = []
+	end
+
+	def name
+		"#{first_name} #{last_name}"
+	end
+
+	def enroll(new_course)
+		return if self.courses.include?(new_course)
+		rasie "error" if has_conflict?(new_course)
+
+		@courses << new_course
+		new_course.students << self
+	end
+
+	def course_load
+		result = Hash.new(0)
+
+		self.courses.each do |i|
+			result[i.department] += i.credits
+		end
+		result
+	end
+
+	def has_conflict?(new_course)
+		self.courses.any? do |i|
+			new_course.conflicts_with?(i)
+		end
+	end
+end
---
+++ Week2/Day4/lib/board.rb
@@ -0,0 +1,249 @@
+# # # class Board
+# # # 	attr_reader :grid, :marks
+
+# # # 	def self.blank_grid
+# # # 		Array.new(3){Array.new(3)}
+# # # 	end
+
+# # # 	def initialize(grid=Board.blank_grid)
+# # # 		@grid = grid
+# # # 		@marks = [:X, :O]
+# # # 	end
+
+# # # 	def [](x, y)
+# # # 		grid[x][y]
+# # # 	end
+
+# # # 	def []=(x, y, value)
+# # # 		grid[x][y] = value
+# # # 	end
+
+# # # 	def place_mark(pos, mark)
+# # # 		self[*pos] = mark
+# # # 	end
+
+
+# # # 	def empty?(pos)
+# # # 		self[*pos].nil?
+# # # 	end
+
+# # # 	def winner
+# # # 		(grid + cols + diagonals).each do |i|
+# # # 			return :X if i == [:X, :X, :X]
+# # # 			return :O if i == [:O, :O, :O]
+# # # 		end
+# # # 		nil
+# # # 	end
+
+# # # 	def cols
+# # # 		result = [[],[],[]]
+# # # 		grid.each do |row|
+# # # 			row.each_with_index do |mark, i|
+# # # 				result[i] << mark
+# # # 			end
+# # # 		end
+# # # 		result
+# # # 	end
+
+# # # 	def diagonals
+# # # 		up_diag = [[0,2],[1,1],[2,0]]
+# # # 		down_diag = [[0,0],[1,1],[2,2]]
+
+# # # 		[down_diag, up_diag].collect do |i|
+# # # 			i.collect{|row,col| grid[row][col]}
+# # # 		end
+# # # 	end
+
+
+# # # 	def over?
+# # # 		grid.flatten.none? {|i| i.nil?} || winner
+# # # 	end
+# # # end
+
+# # class Board
+
+# # 	attr_reader :grid, :marks
+
+
+# # 	def self.make_grid
+# # 		Array.new(3){Array.new(3)}
+# # 	end
+
+# # 	def initialize(grid=Board.make_grid)
+# # 		@grid = grid
+# # 		@marks = [:X, :O]
+# # 	end
+
+# # 	def place_mark(pos, mark)
+# # 		self[*pos] = mark
+# # 	end
+
+# # 	def []=(x, y, mark)
+# # 		grid[x][y] = mark
+# # 	end
+
+# # 	def [](x, y)
+# # 		grid[x][y]
+# # 	end
+
+# # 	def empty?(pos)
+# # 		self[*pos].nil?
+# # 	end
+
+# # 	def winner
+# # 		(grid + col + diagonal).each do |i|
+# # 			return :X if i == [:X, :X, :X]
+# # 			return :O if i == [:O, :O, :O]
+# # 		end
+# # 		nil
+# # 	end
+
+# # 	def col
+# # 		result = [[],[],[]]
+
+# # 		grid.each do |row|
+# # 			row.each_with_index do |m, i|
+# # 				result[i] << m
+# # 			end
+# # 		end
+# # 		result
+# # 	end
+
+# # 	def diagonal
+# # 		up_diag = [[0,2],[1,1],[2,0]]
+# # 		down_diag = [[0,0],[1,1],[2,2]]
+
+# # 		[down_diag, up_diag].collect do |i|
+# # 			i.collect{|row, col| grid[row][col]}
+# # 		end
+# # 	end
+
+# # 	def over?
+# # 		grid.flatten.none?(&:nil?) || winner
+# # 	end
+
+# # end
+
+# class Board
+# 	attr_reader :grid, :mark
+	
+
+# 	def self.make_grid
+# 		Array.new(3) { Array.new(3) }
+# 	end
+
+# 	def initialize(grid=Board.make_grid)
+# 		@grid = grid
+# 		@mark = [:X, :O]
+# 	end
+
+# 	def [](x, y)
+# 		grid[x][y]
+# 	end
+
+# 	def []=(x,y,value)
+# 		grid[x][y] = value
+# 	end
+
+# 	def empty?(pos)
+# 		self[*pos].nil?
+# 	end
+
+# 	def over?
+# 		grid.flatten.none?(&:nil?) || winner
+# 	end
+
+# 	def place_mark(pos, mark)
+# 		self[*pos] = mark
+# 	end
+
+# 	def winner
+# 		(grid + cols + diganols).each do |i|
+# 			return :X if i == [:X, :X, :X]
+# 			return :O if i == [:O, :O, :O]
+# 		end
+# 		nil
+# 	end
+
+# 	def cols
+# 		result = [[],[],[]]
+
+# 		grid.each do |row|
+# 			row.each_with_index do |mark, i|
+# 				result[i] << mark
+# 			end
+# 		end
+# 		result
+# 	end
+
+# 	def diganols
+# 		up = [[0,2],[1,1],[2,0]]
+# 		down = [[0,0],[1,1],[2,2]]
+
+# 		[up,down].collect do |i|
+# 			i.collect{|x,y| grid[x][y]}
+# 		end
+# 	end
+# end
+
+class Board
+	attr_reader :grid, :marks
+
+	def self.make_grid
+		Array.new(3) { Array.new(3)  }
+	end
+
+	def initialize(gird = Board.make_grid)
+		@grid = gird
+		@marks = [:X, :O]
+	end
+
+	def over?
+		grid.flatten.none?(&:nil?) || winner
+	end
+
+	def winner
+		(grid + col + diagnols).each do |i|
+			return :X if i == [:X, :X, :X]
+			return :O if i == [:O, :O, :O]
+		end
+		nil
+	end
+
+	def col
+		result = [[],[],[]]
+		
+		grid.each do |row|
+			row.each_with_index do |col, i|
+				result[i] << col
+			end
+		end
+		result
+	end
+
+	def diagnols
+		up = [[0,2],[1,1],[2,0]]
+		down = [[0,0],[1,1],[2,2]]
+
+		[up,down].collect do |i|
+			i.collect {|x, y| grid[x][y]}
+		end
+	end
+
+	def [](x,y)
+		grid[x][y]
+	end
+
+	def []=(x,y, value)
+		grid[x][y] = value
+	end
+
+	def place_mark(pos, mark)
+		self[*pos] = mark
+	end
+
+	def empty?(pos)
+		self[*pos].nil?
+	end
+
+end
